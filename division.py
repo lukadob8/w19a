bbc03f32da857495b86cf96986e63a3923f47b3e
Luka Dobrijevic <lukadob@Lukas-MacBook-Pro.local>
2020-10-30 18:50:49 -0600

    Added error check for dividing by 0

--- division.py
+++ division.py
@@ -1,2 +1,7 @@
 def divide(numberOne, numberTwo):
+    try:
+        (int(numberOne) / int(numberTwo))
+    except ZeroDivisionError:
+        return("You cannot divide by zero")
     return(int(numberOne) / int(numberTwo))
+  
